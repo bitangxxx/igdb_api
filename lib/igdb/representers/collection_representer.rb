82dd7ddf15b9adab7343df1233b987924c5e5754
Adrian Kovacs <bitangxxx@gmail.com>
2017-06-02 04:49:52 +0200

    representers

--- lib/igdb/representers/collection_representer.rb
+++ lib/igdb/representers/collection_representer.rb
@@ -1,5 +1,12 @@
 module Igdb::CollectionRepresenter
   include Igdb::BaseRepresenter
 
+  property :id
+  property :name
+  property :slug
+  property :url
+  property :created_at
+  property :updated_at
+
   collection :games
 end
--- lib/igdb/representers/theme_representer.rb
+++ lib/igdb/representers/theme_representer.rb
@@ -1,5 +1,12 @@
 module Igdb::ThemeRepresenter
   include Igdb::BaseRepresenter
 
+  property :id
+  property :name
+  property :slug
+  property :url
+  property :created_at
+  property :updated_at
+
   collection :games
 end
